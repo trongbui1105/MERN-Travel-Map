ba3d0408192e6497b664f2f09abd993e71b01045
trongbui1105 <buiquoctrong110500@gmail.com>
2022-01-02 23:17:34 +0700

    MERN App

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,31 @@
+import { useState } from "react";
+import ReactMapGL, { Marker } from "react-map-gl";
+import { Room } from '@material-ui/icons';
 
 function App() {
+  const [viewport, setViewport] = useState({
+    width: "100vw",
+    height: "100vh",
+    latitude: 46,
+    longitude: 17,
+    zoom: 4,
+  });
   return (
     <div className="App">
-      hello
+      <ReactMapGL
+        {...viewport}
+        mapboxApiAccessToken={process.env.REACT_APP_MAPBOX}
+        onViewportChange={(nextViewport) => setViewport(nextViewport)}
+      >
+        <Marker
+          latitude={48.858093}
+          longitude={2.294694}
+          offsetLeft={-20}
+          offsetTop={-10}
+        >
+          <Room style={{fontSize: viewport.zoom * 7, color: "slateblue"}}/>
+        </Marker>
+      </ReactMapGL>
     </div>
   );
 }
